956c79d873b209dc6b6e5995b40509c43d897ca8
Armon Dadgar <armon.dadgar@gmail.com>
2014-07-02 18:28:33 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,230 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+// WatchRE is used to parse a backend configuration. The config should
+// look like "backend=tag.service.datacenter:port". However, the tag, port and
+// datacenter are optional, so it can also be provided as "backend=service"
+var WatchRE = regexp.MustCompile("^([^=]+)=([^.]+\\.)?([^.:]+)(\\.[^.:]+)?(:[0-9]+)?$")
+
+// WatchPath represents a path we need to watch
+type WatchPath struct {
+	Spec       string
+	Backend    string
+	Service    string
+	Tag        string
+	Datacenter string
+	Port       int
+}
+
+// Config is used to configure the HAProxy connector
+type Config struct {
+	// DryRun is used to avoid actually modifying the file
+	// or reloading HAProxy.
+	DryRun bool
+
+	// Address is the Consul HTTP API address
+	Address string `mapstructure:"address"`
+
+	// Path to the HAProxy template file
+	Template string `mapstructure:"tempalte"`
+
+	// Path to the HAProxy configuration file to write
+	Path string `mapstructure:"path"`
+
+	// Command used to reload HAProxy
+	ReloadCommand string `mapstructure:"reload_command"`
+
+	// Backends are used to specify what we watch. Given as:
+	// "name=(tag.)service"
+	Backends []string `mapstructure:"backends"`
+
+	// watches are the watches we need to track
+	watches []*WatchPath
+}
+
+func main() {
+	os.Exit(realMain())
+}
+
+// realMain is the actual entry point, but we wrap it to set
+// a proper exit code on return
+func realMain() int {
+	var configFile string
+	var backends []string
+	conf := &Config{}
+	flag.Usage = usage
+	flag.StringVar(&conf.Address, "addr", "127.0.0.1:8500", "consul HTTP API address with port")
+	flag.StringVar(&conf.Template, "template", "", "template path")
+	flag.StringVar(&conf.Path, "path", "", "config path")
+	flag.StringVar(&conf.ReloadCommand, "reload", "", "reload command")
+	flag.StringVar(&configFile, "f", "", "config file")
+	flag.BoolVar(&conf.DryRun, "dry", false, "dry run")
+	flag.Var((*AppendSliceValue)(&backends), "backend", "backend to populate")
+	flag.Parse()
+
+	// Parse the configuration file if given
+	if configFile != "" {
+		if err := readConfig(configFile, conf); err != nil {
+			log.Printf("[ERR] Failed to read config file: %v", err)
+			return 1
+		}
+	}
+
+	// Merge the backends together
+	conf.Backends = append(conf.Backends, backends...)
+
+	// Sanity check the configuration
+	if errs := validateConfig(conf); len(errs) != 0 {
+		for _, err := range errs {
+			log.Printf("[ERR] %v", err)
+		}
+		return 1
+	}
+
+	// Start watching for changes
+	stopCh, finishCh := watch(conf)
+
+	// Wait for termination
+	return waitForTerm(conf, stopCh, finishCh)
+}
+
+// readConfig is used to read a configuration file
+func readConfig(path string, config *Config) error {
+	// Read the file
+	contents, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	// Decode the file
+	var raw interface{}
+	if err := json.NewDecoder(bytes.NewReader(contents)).Decode(&raw); err != nil {
+		return err
+	}
+
+	// Map to our output
+	if err := mapstructure.Decode(raw, config); err != nil {
+		return err
+	}
+	return nil
+}
+
+// validateConfig is used to sanity check the configuration
+func validateConfig(conf *Config) (errs []error) {
+	// Check the template
+	if conf.Template == "" {
+		errs = append(errs, errors.New("missing template path"))
+	} else {
+		_, err := ioutil.ReadFile(conf.Template)
+		if err != nil {
+			errs = append(errs, fmt.Errorf("failed to read template: %v", err))
+		}
+	}
+
+	if conf.Path == "" {
+		errs = append(errs, errors.New("missing configuration path"))
+	}
+
+	if conf.ReloadCommand == "" {
+		errs = append(errs, errors.New("missing reload command"))
+	}
+
+	if len(conf.Backends) == 0 {
+		errs = append(errs, errors.New("missing backends to populate"))
+	}
+
+	for _, b := range conf.Backends {
+		parts := WatchRE.FindStringSubmatch(b)
+		if parts == nil || len(parts) != 6 {
+			errs = append(errs, fmt.Errorf("Backend '%s' could not be parsed", b))
+			continue
+		}
+		var port int
+		if parts[5] != "" {
+			p, err := strconv.ParseInt(strings.TrimPrefix(parts[5], ":"), 10, 64)
+			if err != nil {
+				errs = append(errs, fmt.Errorf("Backend '%s' port could not be parsed", b))
+				continue
+			}
+			port = int(p)
+		}
+		wp := &WatchPath{
+			Spec:       parts[0],
+			Backend:    parts[1],
+			Tag:        strings.TrimSuffix(parts[2], "."),
+			Service:    parts[3],
+			Datacenter: strings.TrimPrefix(parts[4], "."),
+			Port:       port,
+		}
+		conf.watches = append(conf.watches, wp)
+	}
+
+	return
+}
+
+// waitForTerm waits until we receive a signal to exit
+func waitForTerm(conf *Config, stopCh, finishCh chan struct{}) int {
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGHUP)
+	for {
+		select {
+		case sig := <-signalCh:
+			switch sig {
+			case syscall.SIGHUP:
+				// TODO: Handle reload
+				log.Printf("[WARN] SIGHUP received, reloading configuration...")
+
+				// Stop the existing watcher
+				close(stopCh)
+
+				// Start a new watcher
+				stopCh, finishCh = watch(conf)
+
+			default:
+				log.Printf("[WARN] Received %v signal, shutting down", sig)
+				return 0
+			}
+		case <-finishCh:
+			if conf.DryRun {
+				return 0
+			}
+			log.Printf("[WARN] Aborting watching for changes, shutting down")
+			return 1
+		}
+	}
+	return 0
+}
+
+func usage() {
+	cmd := filepath.Base(os.Args[0])
+	fmt.Fprintf(os.Stderr, strings.TrimSpace(helpText)+"\n\n", cmd)
+}
+
+const helpText = `
+Usage: %s [options]
+
+  Watches a service group in Consul and dynamically configures
+  an HAProxy backend.
+
+Options:
+
+  -addr=127.0.0.1:8500  Provides the HTTP address of a Consul agent.
+`
---
+++ watch.go
@@ -0,0 +1,311 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"reflect"
+	"runtime"
+	"strings"
+	"sync"
+	"text/template"
+	"time"
+
+	"github.com/armon/consul-api"
+)
+
+const (
+	// failSleep controls how long to sleep on a failure
+	failSleep = 5 * time.Second
+
+	// maxFailures controls the maximum number of failures
+	// before we limit the sleep value
+	maxFailures = 3
+
+	// waitTime is used to control how long we do a blocking
+	// query for
+	waitTime = 60 * time.Second
+)
+
+type backendData struct {
+	sync.Mutex
+
+	// Client is a shared Consul client
+	Client *consulapi.Client
+
+	// Servers maps each watch path to a list of entries
+	Servers map[*WatchPath][]*consulapi.ServiceEntry
+
+	// Backends maps a backend to a list of watch paths used
+	// to build up the server list
+	Backends map[string][]*WatchPath
+
+	// ChangeCh is used to inform of an update
+	ChangeCh chan struct{}
+
+	// StopCh is used to trigger a stop
+	StopCh chan struct{}
+}
+
+// watch is used to start a long running watcher to handle updates.
+// Returns a stopCh, and a finishCh.
+func watch(conf *Config) (chan struct{}, chan struct{}) {
+	stopCh := make(chan struct{})
+	finishCh := make(chan struct{})
+	go runWatch(conf, stopCh, finishCh)
+	return stopCh, finishCh
+}
+
+// runWatch is a long running routine that watches with a
+// given configuration
+func runWatch(conf *Config, stopCh, doneCh chan struct{}) {
+	defer close(doneCh)
+
+	// Create the consul client
+	consulConf := consulapi.DefaultConfig()
+	if conf.Address != "" {
+		consulConf.Address = conf.Address
+	}
+
+	// Attempt to contact the agent
+	client, err := consulapi.NewClient(consulConf)
+	if err != nil {
+		log.Printf("[ERR] Failed to initialize consul client: %v", err)
+		return
+	}
+	if _, err := client.Agent().NodeName(); err != nil {
+		log.Printf("[ERR] Failed to contact consul agent: %v", err)
+		return
+	}
+
+	// Create a backend store
+	data := &backendData{
+		Client:   client,
+		Servers:  make(map[*WatchPath][]*consulapi.ServiceEntry),
+		Backends: make(map[string][]*WatchPath),
+		ChangeCh: make(chan struct{}, 1),
+		StopCh:   stopCh,
+	}
+
+	// Start the watches
+	data.Lock()
+	for _, watch := range conf.watches {
+		data.Backends[watch.Backend] = append(data.Backends[watch.Backend], watch)
+		go runSingleWatch(conf, data, watch)
+	}
+	data.Unlock()
+
+	// Monitor for changes or stop
+	for {
+		select {
+		case <-data.ChangeCh:
+			if maybeRefresh(conf, data) {
+				return
+			}
+
+		case <-stopCh:
+			return
+		}
+	}
+}
+
+// maybeRefresh is used to handle a potential config update
+func maybeRefresh(conf *Config, data *backendData) (exit bool) {
+	// Ignore initial updates until all the data is ready
+	data.Lock()
+	num := len(data.Servers)
+	data.Unlock()
+	if num < len(conf.watches) {
+		return
+	}
+
+	// Merge the data for each backend
+	backendServers := make(map[string][]*consulapi.ServiceEntry)
+	data.Lock()
+	for backend, watches := range data.Backends {
+		var all []*consulapi.ServiceEntry
+		for _, watch := range watches {
+			entries := data.Servers[watch]
+			all = append(all, entries...)
+		}
+		backendServers[backend] = all
+	}
+	data.Unlock()
+
+	// Format the output
+	outVars := formatOutput(backendServers)
+
+	// Read the template
+	raw, err := ioutil.ReadFile(conf.Template)
+	if err != nil {
+		log.Printf("[ERR] Failed to read template: %v", err)
+		return true
+	}
+
+	// Create the template
+	templ, err := template.New("output").Parse(string(raw))
+	if err != nil {
+		log.Printf("[ERR] Failed to parse the template: %v", err)
+		return true
+	}
+
+	// Generate the output
+	var output bytes.Buffer
+	if err := templ.Execute(&output, outVars); err != nil {
+		log.Printf("[ERR] Failed to generate the template: %v", err)
+		return true
+	}
+
+	// Check for a dry run
+	if conf.DryRun {
+		fmt.Printf("%s\n", output.Bytes())
+		return true
+	}
+
+	// Write out the configuration
+	if err := ioutil.WriteFile(conf.Path, output.Bytes(), 0660); err != nil {
+		log.Printf("[ERR] Failed to write config file: %v", err)
+		return true
+	}
+	log.Printf("[INFO] Updated configuration file at %s", conf.Path)
+
+	// Invoke the reload hook
+	if err := reload(conf); err != nil {
+		log.Printf("[ERR] Failed to reload: %v", err)
+	} else {
+		log.Printf("[INFO] Completed reload")
+	}
+	return
+}
+
+// runSingleWatch is used to query a single watch path for changes
+func runSingleWatch(conf *Config, data *backendData, watch *WatchPath) {
+	health := data.Client.Health()
+	opts := &consulapi.QueryOptions{
+		WaitTime: waitTime,
+	}
+	if watch.Datacenter != "" {
+		opts.Datacenter = watch.Datacenter
+	}
+
+	failures := 0
+	for {
+		if shouldStop(data.StopCh) {
+			return
+		}
+		entries, qm, err := health.Service(watch.Service, watch.Tag, true, opts)
+		if err != nil {
+			log.Printf("[ERR] Failed to fetch service nodes: %v", err)
+		}
+
+		// Fixup the ports if necessary
+		if watch.Port != 0 {
+			for _, entry := range entries {
+				if entry.Service.Port == 0 {
+					entry.Service.Port = watch.Port
+				}
+			}
+		}
+
+		// Update the entries. If this is the first read, do it on error
+		data.Lock()
+		old, ok := data.Servers[watch]
+		if !ok || (err == nil && !reflect.DeepEqual(old, entries)) {
+			data.Servers[watch] = entries
+			asyncNotify(data.ChangeCh)
+			if !conf.DryRun {
+				log.Printf("[DEBUG] Updated nodes for %v", watch.Spec)
+			}
+		}
+		data.Unlock()
+
+		// Stop immediately on a dry run
+		if conf.DryRun {
+			return
+		}
+
+		// Check for an error
+		if err != nil {
+			failures = min(failures+1, maxFailures)
+			time.Sleep(backoff(failSleep, failures))
+		} else {
+			failures = 0
+			opts.WaitIndex = qm.LastIndex
+		}
+	}
+}
+
+// reload is used to invoke the reload command
+func reload(conf *Config) error {
+	// Determine the shell invocation based on OS
+	var shell, flag string
+	if runtime.GOOS == "windows" {
+		shell = "cmd"
+		flag = "/C"
+	} else {
+		shell = "/bin/sh"
+		flag = "-c"
+	}
+
+	// Create and invoke the command
+	cmd := exec.Command(shell, flag, conf.ReloadCommand)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// shouldStop checks for a closed control channel
+func shouldStop(ch chan struct{}) bool {
+	select {
+	case <-ch:
+		return true
+	default:
+		return false
+	}
+}
+
+// asyncNotify is used to notify a channel
+func asyncNotify(ch chan struct{}) {
+	select {
+	case ch <- struct{}{}:
+	default:
+	}
+}
+
+// min returns the min of two ints
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// backoff is used to compute an exponential backoff
+func backoff(interval time.Duration, times int) time.Duration {
+	base := interval
+	for ; times > 1; times-- {
+		base *= interval
+	}
+	return interval
+}
+
+// formatOutput converts the service entries into a format
+// suitable for templating into the HAProxy file
+func formatOutput(inp map[string][]*consulapi.ServiceEntry) map[string]string {
+	out := make(map[string]string)
+	for backend, entries := range inp {
+		servers := make([]string, len(entries))
+		for idx, entry := range entries {
+			name := fmt.Sprintf("%s_%s", entry.Node.Node, entry.Service.ID)
+			ip := net.ParseIP(entry.Node.Address)
+			addr := &net.TCPAddr{IP: ip, Port: entry.Service.Port}
+			servers[idx] = fmt.Sprintf("\tserver %s %s\n", name, addr)
+		}
+		out[backend] = strings.Join(servers, "")
+	}
+	return out
+}
